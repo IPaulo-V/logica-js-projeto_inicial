ee1e43808604dea5a4f00548c038d23f3110c660
IPaulo-V <paulo.victor_sv@proton.me>
2025-09-26 21:31:03 -0300

    introdução while

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 alert('Boas Vindas ao jogo do número aleatório');
-let numeroSecreto = 1;
+let numeroSecreto = 2;
 console.log(numeroSecreto)
-let chute = prompt('Escolha um número entre 1 e 10');
-//Se o chute for igual ao número secreto
-if(chute == numeroSecreto) {
-    alert(`Isso ai! você descobriu o número secreto ${numeroSecreto}`);
-}
-//Se o chute for maior que número secreto
-if(chute > numeroSecreto) {
-    alert('Você errou o numero é menor');
-}
-//Se o chute for menor que número secreto
-if(chute < numeroSecreto) {
-    alert('Você errou o numero é maior');
+let chute
+//enquanto chute não for igual ao número secreto
+while(chute != numeroSecreto){
+    chute = prompt('Escolha um número entre 1 e 10');
+    //Se o chute for igual ao número secreto
+    if(chute == numeroSecreto) {
+        alert(`Isso ai! você descobriu o número secreto ${numeroSecreto}`);
+    }
+    //Se o chute for maior que número secreto
+    if(chute > numeroSecreto) {
+        alert('Você errou o numero é menor');
+    }
+    //Se o chute for menor que número secreto
+    if(chute < numeroSecreto) {
+        alert('Você errou o numero é maior');
+    }
 }
