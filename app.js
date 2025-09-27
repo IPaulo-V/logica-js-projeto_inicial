ad8b422bd8df8a5406b11ef4c9fee3f00d8b08bd
IPaulo-V <paulo.victor_sv@proton.me>
2025-09-26 21:40:53 -0300

    # de tentativas

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 alert('Boas Vindas ao jogo do número aleatório');
 let numeroSecreto = 2;
-console.log(numeroSecreto)
-let chute
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1
 //enquanto chute não for igual ao número secreto
 while(chute != numeroSecreto){
     chute = prompt('Escolha um número entre 1 e 10');
@@ -12,9 +13,14 @@ while(chute != numeroSecreto){
     //Se o chute for maior que número secreto
     if(chute > numeroSecreto) {
         alert('Você errou o numero é menor');
+        tentativas++
     }
     //Se o chute for menor que número secreto
     if(chute < numeroSecreto) {
         alert('Você errou o numero é maior');
+        tentativas++
+    }
+    if (chute == numeroSecreto){
+        alert(`você acertou em ${tentativas} tentativas! Parabéns`)
     }
 }
