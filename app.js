952e4f952f6781e2be6902a89200790643c89258
IPaulo-V <paulo.victor_sv@proton.me>
2025-09-28 19:35:46 -0300

    novas features

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 alert('Boas Vindas ao jogo do número aleatório');
-let numeroSecreto = 2;
+let numeroSecreto = parseInt(Math.random() * 100+1);
 let chute;
-let tentativas = 1
+let tentativas = 1;
 //enquanto chute não for igual ao número secreto
 while(chute != numeroSecreto){
-    chute = prompt('Escolha um número entre 1 e 10');
+    chute = prompt('Escolha um número entre 1 e 100');
     let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
     //Se o chute for igual ao número secreto
    if (chute == numeroSecreto){
-            alert(`Isso ai! você descobriu o número secreto ${numeroSecreto}!\n
-                Você acertou em ${tentativas} ${palavraTentativa}! Parabéns!`);
+            alert(`Isso ai! você descobriu o número secreto ${numeroSecreto}!\n Você acertou em ${tentativas} ${palavraTentativa}! Parabéns!`);
     }
     //Se o chute for maior que número secreto
     else if(chute > numeroSecreto) {
